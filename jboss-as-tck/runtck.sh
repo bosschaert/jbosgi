194086668d42cc4abdab570f8f9c11b7a2a8f41f
David Bosschaert <david.bosschaert@gmail.com>
2012-11-26 16:22:44 +0000

    Wip

--- jboss-as-tck/runtck.sh
+++ jboss-as-tck/runtck.sh
@@ -1,10 +1,10 @@
-if [ "$2" == "" ] 
+if [ "$3" == "" ] 
 then
-    echo Please provide the jboss-as build location, and the location of the 
-    echo jboss-as-osgi-launcher.jar file, e.g.
-    echo  $0 /jboss-as/jboss-as-7.2.0 /jboss-as/osgi/launcher/target/jboss-as-osgi-launcher-7.2.0.jar
+    echo Please provide the OSGi TCK location, the jboss-as build location, and the
+    echo location of the jboss-as-osgi-launcher.jar file, e.g.
+    echo  $0 ./osgitck/ /jboss-as/jboss-as-7.2.0 /jboss-as/osgi/launcher/target/jboss-as-osgi-launcher-7.2.0.jar
 else
-    if [ ! -d "$1/modules" ]
+    if [ ! -d "$2/modules" ]
     then
         echo Please specify a valid jboss-as build directory.
         echo It must contain the modules subdirectory.
@@ -14,9 +14,9 @@ pushd `dirname $0`
 CURDIR=`pwd`
 popd
 
-export TCKCHECKOUT=$CURDIR/r4v42tck
-export JBOSS_AS_LOCATION=$1
-export JBOSS_OSGI_LAUNCHER=$2
+export TCKCHECKOUT=$1
+export JBOSS_AS_LOCATION=$2
+export JBOSS_OSGI_LAUNCHER=$3
 
 export JBOSS_AS_EMBEDDED=`find $JBOSS_AS_LOCATION | grep "\/jboss-as-embedded.*[.]jar$"`
 export JBOSS_MODULES=`find $JBOSS_AS_LOCATION | grep "\/jboss-modules.*[.]jar$"`
@@ -29,16 +29,6 @@ export JBOSS_OSGI_CORE=`find $JBOSS_AS_LOCATION | grep "\/org[.]osgi[.]core.*[.]
 
 echo TCK Checkout directory: $TCKCHECKOUT
 echo JBOSS_AS_LOCATION: $JBOSS_AS_LOCATION
-
-
-# Clone the TCK repo 
-if [ ! -d $TCKCHECKOUT ]; then
-  svn co https://svn.devel.redhat.com/repos/jboss-tck/osgitck/r4v42 $TCKCHECKOUT
-  #git clone /home/hudson/static_build_env/osgi-tck-4.2/r4v42 $TCKCHECKOUT
-fi
-
-# Switch to known tck-checkout tag 
-# cd $TCKCHECKOUT; git checkout r4v42-core-cmpn-final
 
 # Setup the TCK
 ant setup.vi
